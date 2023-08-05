7d82439c3a401853787b5c08031ab26f71328a7f
Abduljebar Sani <Abduljebar49@gmail.com>
2023-08-05 13:07:49 +0300

    final bug fix

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,12 +1,36 @@
 import { AfterViewInit, Component, Input, OnInit } from "@angular/core";
 import { TabComponent } from "../tab/tab.component";
+import { NgbNavConfig, NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "tabs",
   templateUrl: "./tabs.component.html",
+  styles: [
+    `
+      .active {
+        background-color: #009da0 !important;
+        color: white !important;
+        --bs-nav-tabs-link-active-border-color: #009da0 #009da0 #ededed !important;
+      }
+
+      .nav-tabs {
+        --bs-nav-tabs-border-width: 2px !important;
+        --bs-nav-tabs-border-color: #009da0 !important;
+        --bs-nav-tabs-border-radius: 0.375rem;
+        color: #009da0;
+      }
+    `,
+  ],
 })
-
-
+// <a
+//     *ngFor="let tab of tabs; let i = index"
+//     (click)="selectTab(tab)"
+//     class=""
+//     [ngClass]="i == 0? 'index-margin':''"
+//     [ngClass]="tab.active ? 'tab-btn btn-primary' : 'btn-active btn-outline-primary'"
+//   >
+//     {{ tab.tabTitle }}
+//   </a>
 export class TabsComponent {
   tabs: TabComponent[] = [];
   @Input() active: number = 0;
@@ -21,6 +45,7 @@ export class TabsComponent {
   }
 
   onShown($event:any) {
+    console.log("inside on shown : ")
     for (let i = 0; i < this.tabs.length; i++) {
       if ($event == i) {
         this.tabs[i].show = true;
@@ -28,5 +53,6 @@ export class TabsComponent {
         this.tabs[i].show = false;
       }
     }
+    console.log("tabs ",this.tabs)
   }
 }
